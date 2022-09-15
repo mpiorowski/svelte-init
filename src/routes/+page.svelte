10ccd78f612530241f757021774b91b2a36b0470
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-09-15 13:43:45 +0200

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import { fade } from 'svelte/transition';
-  import { LogoIcon } from '../lib';
 </script>
 
 <div in:fade class="content">
-  <img src="/logo.png" width="300">
+  <img src="/logo.png" width="300" alt="SvelteInit" />
 
   <h2>
     A low-overhead Svelte UI library with the basic, most useful components and
