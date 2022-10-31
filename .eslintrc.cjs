8849be392ac35ebe43d4fee6b3437845185ba6b0
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-10-31 19:28:49 +0100

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
     ],
     plugins: ['svelte3', '@typescript-eslint'],
     ignorePatterns: ['*.cjs'],
