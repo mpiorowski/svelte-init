dca48b0d3f3816769064a13409b47a84c2ad6883
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-11-20 20:44:36 +0100

    cleaning-confirmation-modal

--- src/lib/components/modal/confirmation.modal.svelte
+++ src/lib/components/modal/confirmation.modal.svelte
@@ -4,15 +4,16 @@
     export let size: 'sm' | 'md' | 'lg' = 'md';
     export let actionBtn: 'primary' | 'ghost' | 'error' = 'error';
     export let confirmBtm: 'primary' | 'ghost' | 'error' = 'error';
-    export let action: () => void;
+    export let action: () => Promise<void> | void;
     export let confirmText = 'Confirm';
     export let cancelText = 'Cancel';
 
     let isOpen = false;
+    let loading = false;
 </script>
 
 <Button on:click={() => (isOpen = true)} type={actionBtn}>
-        <slot name="button" />
+    <slot name="button" />
 </Button>
 
 <Modal {isOpen} onClose={() => (isOpen = false)} {size}>
@@ -23,6 +24,17 @@
         <Button on:click={() => (isOpen = false)} type="ghost">
             {cancelText}
         </Button>
-        <Button on:click={action} type={confirmBtm}>{confirmText}</Button>
+        <Button
+            on:click={async () => {
+                loading = true;
+                await action();
+                loading = false;
+                isOpen = false;
+            }}
+            type={confirmBtm}
+            {loading}
+        >
+            {confirmText}
+        </Button>
     </svelte:fragment>
 </Modal>
