2cc920e241fc5bd62b36a4a9ef4636e3a2a125fb
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-11-20 20:36:37 +0100

    update

--- src/lib/components/modal/confirmation.modal.svelte
+++ src/lib/components/modal/confirmation.modal.svelte
@@ -2,15 +2,16 @@
     import { Button, Modal } from '$lib';
 
     export let size: 'sm' | 'md' | 'lg' = 'md';
-    export let type: 'primary' | 'ghost' | 'error' = 'error';
+    export let actionBtn: 'primary' | 'ghost' | 'error' = 'error';
+    export let confirmBtm: 'primary' | 'ghost' | 'error' = 'error';
     export let action: () => void;
-    export let confirmText = 'common.confirm';
-    export let cancelText = 'common.cancel';
+    export let confirmText = 'Confirm';
+    export let cancelText = 'Cancel';
 
     let isOpen = false;
 </script>
 
-<Button on:click={() => (isOpen = true)} {type}>
+<Button on:click={() => (isOpen = true)} type={actionBtn}>
         <slot name="button" />
 </Button>
 
@@ -22,6 +23,6 @@
         <Button on:click={() => (isOpen = false)} type="ghost">
             {cancelText}
         </Button>
-        <Button on:click={action}>{confirmText}</Button>
+        <Button on:click={action} type={confirmBtm}>{confirmText}</Button>
     </svelte:fragment>
 </Modal>
