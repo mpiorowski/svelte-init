677bcf2d493f9a5da95186df9234aa1101db0b65
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-12-24 10:21:30 +0100

    finishedc

--- src/routes/tooltip/+page.svelte
+++ src/routes/tooltip/+page.svelte
@@ -9,27 +9,16 @@
     <h1>Tooltip</h1>
     <InputNumber bind:value={move} label="Move tooltip" />
     <Tooltip tooltip={'tooltip text'} position={'top'} {move}>
-        <div class="tooltip-container">Top</div>
+        <div class="border border-gray-300 bg-gray-100 p-2 w-20 text-center rounded">Top</div>
     </Tooltip>
     <Tooltip tooltip={'tooltip text'} {move} position={'right'}>
-        <div class="tooltip-container">Right</div>
+        <div class="border border-gray-300 bg-gray-100 p-2 w-20 text-center rounded">Right</div>
     </Tooltip>
     <Tooltip tooltip={'tooltip text'} position={'left'} {move}>
-        <div class="tooltip-container">Left</div>
+        <div class="border border-gray-300 bg-gray-100 p-2 w-20 text-center rounded">Left</div>
     </Tooltip>
     <Tooltip tooltip={'tooltip text'} position={'bottom'} {move}>
-        <div class="tooltip-container">Bottom</div>
+        <div class="border border-gray-300 bg-gray-100 p-2 w-20 text-center rounded">Bottom</div>
     </Tooltip>
 </div>
 
-<style>
-    .tooltip-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 0.5rem;
-        width: 5rem;
-        height: 2.5rem;
-        @apply bg-gray-200;
-    }
-</style>
