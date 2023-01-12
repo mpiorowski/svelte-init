2e58650c7acda2276156ffe092a3e349d705ed9b
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-01-13 00:25:33 +0100

    update

--- src/lib/components/form/checkbox.svelte
+++ src/lib/components/form/checkbox.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    export let checked: boolean;
+    export let checked = false;
     export let name = '';
     export let onChange: (() => void) | undefined = undefined;
 </script>
